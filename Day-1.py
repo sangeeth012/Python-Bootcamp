b35cf850267f5e1293f3003cfa52107c34972e4a
sangeeth012 <sangeethra1997@gmail.com>
2025-08-23 20:55:14 +0530

    Working With variable in python

---
+++ Day-1.py
@@ -0,0 +1,17 @@
+print(" hello world ")
+print("1.mix 500g of flour,10g yeast and 300ml water in a bowl.")
+print("2.knead the dough for 10 minutes.")
+print("3.Add 3g of salt.")
+print("4.Leave to raise for 2 hour")
+print("5.Bake at 200 degrees c for 30 minutes")
+print("\n")
+print("1.mix 500g of flour,10g yeast and 300ml water in a bowl.\n2.knead the dough for 10 minutes.\n3.Add 3g of salt.\n4.Leave to raise for 2 hour,\n5.Bake at 200 degrees c for 30 minute")
+
+print("********************************************")
+print("LEARNING STRING CONCODINATION ")
+print("Hello" + "Python")
+print("********************************************")
+
+print("hello " + input("what is your name "))
+print(len("Hello" + " " + input(" What is your name ")))
+
