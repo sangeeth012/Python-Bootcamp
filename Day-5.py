d65ddead938eb328ad857a578383f34f80f72aad
sangeeth012 <sangeethra1997@gmail.com>
2025-08-28 19:13:49 +0530

    maze-solving

---
+++ Day-5.py
@@ -0,0 +1,14 @@
+def turn right():
+turn_left()
+turn_left()
+turn left()
+
+while not at goal():
+ if wall_in front() == True:
+  turn left()
+ if wall in front() != True:
+  move()
+  if wall_in_front() != True:
+   turn_right()
+  else:
+   turn_right()
